fb8faab38ec7b479f7eb80b9e39d64af4f550706
T3rr0or <33265443+T3rr0or@users.noreply.github.com>
2026-05-04 14:33:50 +0200

    chore: release 0.9.2

--- backend/scanr/config.py
+++ backend/scanr/config.py
@@ -13,7 +13,7 @@ class Settings(BaseSettings):
 
     # Application
     app_name: str = "ScanR"
-    app_version: str = "0.9.1"
+    app_version: str = "0.9.2"
     debug: bool = False
     base_dir: Path = Path(__file__).parent.parent
 
